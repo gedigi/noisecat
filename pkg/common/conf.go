e1b10cc1a4535122885b105d656295f00b815e82
gedigi <19227040+gedigi@users.noreply.github.com>
2018-03-02 19:03:18 -0800

    started common configuration

---
+++ pkg/common/conf.go
@@ -0,0 +1,190 @@
+package common
+
+import (
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io/ioutil"
+	"regexp"
+
+	"github.com/gedigi/noise"
+)
+
+// Configuration parameters
+type Configuration struct {
+	SrcPort string
+	SrcHost string
+	DstPort string
+	DstHost string
+
+	ExecuteCmd string
+	Proxy      string
+	Listen     bool
+	Verbose    bool
+	Daemon     bool
+	Keygen     bool
+
+	Protocol   string
+	Pattern    byte
+	DHFunc     byte
+	CipherFunc byte
+	HashFunc   byte
+
+	PSK     string
+	RStatic string
+	LStatic string
+
+	framework string
+}
+
+// ParseConfig parses a configuration struct for setup and correctness
+func (config *Configuration) ParseConfig() (interface{}, error) {
+	var err error
+
+	// Skip all the checks if I only have to generate a keypair
+	if config.Keygen {
+		return nil, nil
+	}
+
+	if config.Daemon {
+		if !config.Listen {
+			return nil, errors.New("-k requires -l")
+		}
+		if config.Proxy == "" && config.ExecuteCmd == "" {
+			return nil, errors.New("-k requires -proxy or -e")
+		}
+	}
+	if config.Proxy != "" && !config.Listen {
+		return nil, errors.New("Client mode doesn't support -proxy")
+	}
+
+	if config.framework == "noise" {
+		noise, err := config.parseNoise()
+		if err != nil {
+			return nil, err
+		}
+		return noise, nil
+	}
+	noisesocket, err := config.parseNoisesocket()
+	if err != nil {
+		return nil, err
+	}
+	return noisesocket, nil
+}
+
+func (config *Configuration) parseNoisesocket() (interface{}, error) {
+	return nil, nil
+}
+
+func (config *Configuration) parseNoise() (interface{}, error) {
+	pattern, dh, cipher, hash, err := parseProtocolName(config.Protocol)
+	if err != nil {
+		return nil, err
+	}
+	cs := noise.NewCipherSuite(DHByteObj[dh], CipherByteObj[cipher], HashByteObj[hash])
+	noiseConf := &noise.Config{
+		CipherSuite: cs,
+		Random:      rand.Reader,
+		Pattern:     PatternByteObj[pattern],
+		Initiator:   !config.Listen,
+	}
+
+	if config.PSK != "" {
+		h := sha256.New()
+		h.Write([]byte(config.PSK))
+		noiseConf.PresharedKey = h.Sum(nil)
+	}
+
+	checkLocalKeypair := func() error {
+		if config.LStatic != "" {
+			k, err := ioutil.ReadFile(config.LStatic)
+			if err != nil {
+				return errors.New("Can't read keyfile")
+			}
+			json.Unmarshal(k, &noiseConf.StaticKeypair)
+		} else {
+			noiseConf.StaticKeypair, err = cs.GenerateKeypair(rand.Reader)
+			if err != nil {
+				return errors.New("Can't generate keys")
+			}
+		}
+		return nil
+	}
+
+	checkRemoteStatic := func() error {
+		if config.RStatic == "" {
+			return errors.New("You need to provide the remote peer static key (-rstatic)")
+		}
+		decodedRStatic, err := base64.StdEncoding.DecodeString(config.RStatic)
+		if err != nil {
+			return errors.New("Invalid remote static key")
+		}
+		if len(decodedRStatic) != 32 {
+			return errors.New("Remote static key needs to be 32 bytes")
+		}
+		noiseConf.PeerStatic = decodedRStatic
+		return nil
+	}
+
+	if noiseConf.Initiator {
+		switch noiseConf.Pattern.Name[0] {
+		case 'X', 'I', 'K':
+			if err = checkLocalKeypair(); err != nil {
+				return nil, err
+			}
+		}
+		switch noiseConf.Pattern.Name[1] {
+		case 'K':
+			if err = checkRemoteStatic(); err != nil {
+				return nil, err
+			}
+		}
+	} else {
+		switch noiseConf.Pattern.Name[0] {
+		case 'K':
+			if err = checkRemoteStatic(); err != nil {
+				return nil, err
+			}
+		}
+		switch noiseConf.Pattern.Name[1] {
+		case 'X', 'K':
+			if err = checkLocalKeypair(); err != nil {
+				return nil, err
+			}
+		}
+	}
+	return noiseConf, nil
+}
+
+func parseProtocolName(protoName string) (byte, byte, byte, byte, error) {
+	var hs, dh, cipher, hash byte
+	var err error
+	var ok bool
+	regEx := regexp.MustCompile(`Noise_(\w{2})_(\w+)_(\w+)_(\w+)`)
+	results := regEx.FindStringSubmatch(protoName)
+	if len(results) == 5 {
+		if hs, ok = PatternStrByte[results[1]]; ok == false {
+			err = errors.New("Invalid handshake pattern")
+			goto exit
+		}
+		if dh, ok = DHStrByte[results[2]]; ok == false {
+			err = errors.New("Invalid DH function")
+			goto exit
+		}
+		if cipher, ok = CipherStrByte[results[3]]; ok == false {
+			err = errors.New("Invalid cipher function")
+			goto exit
+		}
+		if hash, ok = HashStrByte[results[4]]; ok == false {
+			err = errors.New("Invalid hash function")
+			goto exit
+		}
+		err = nil
+	} else {
+		err = errors.New("Invalid protocol name")
+	}
+exit:
+	return hs, dh, cipher, hash, err
+}
---
+++ pkg/common/protocols.go
@@ -0,0 +1,95 @@
+package common
+
+import "github.com/gedigi/noise"
+
+const (
+	NOISE_DH_CURVE25519 = 1
+	NOISE_DH_CURVE448   = 2
+
+	NOISE_CIPHER_CHACHAPOLY = 1
+	NOISE_CIPHER_AESGCM     = 2
+
+	NOISE_HASH_BLAKE2s = 1
+	NOISE_HASH_BLAKE2b = 2
+	NOISE_HASH_SHA256  = 3
+	NOISE_HASH_SHA512  = 4
+
+	NOISE_PATTERN_NN = 4
+	NOISE_PATTERN_NK = 5
+	NOISE_PATTERN_NX = 6
+	NOISE_PATTERN_XN = 7
+	NOISE_PATTERN_XK = 8
+	NOISE_PATTERN_XX = 9
+	NOISE_PATTERN_KN = 10
+	NOISE_PATTERN_KK = 11
+	NOISE_PATTERN_KX = 12
+	NOISE_PATTERN_IN = 13
+	NOISE_PATTERN_IK = 14
+	NOISE_PATTERN_IX = 15
+)
+
+// DH Funcs
+var DHStrByte = map[string]byte{
+	"25519": NOISE_DH_CURVE25519,
+	"448":   NOISE_DH_CURVE448,
+}
+
+var DHByteObj = map[byte]noise.DHFunc{
+	NOISE_DH_CURVE25519: noise.DH25519,
+}
+
+// Hash Funcs
+var HashStrByte = map[string]byte{
+	"BLAKE2s": NOISE_HASH_BLAKE2s,
+	"BLAKE2b": NOISE_HASH_BLAKE2b,
+	"SHA256":  NOISE_HASH_SHA256,
+	"SHA512":  NOISE_HASH_SHA512,
+}
+
+var HashByteObj = map[byte]noise.HashFunc{
+	NOISE_HASH_BLAKE2s: noise.HashBLAKE2s,
+	NOISE_HASH_BLAKE2b: noise.HashBLAKE2b,
+	NOISE_HASH_SHA256:  noise.HashSHA256,
+	NOISE_HASH_SHA512:  noise.HashSHA512,
+}
+
+// Cipher Funcs
+var CipherStrByte = map[string]byte{
+	"ChaChaPoly": NOISE_CIPHER_CHACHAPOLY,
+	"AESGCM":     NOISE_CIPHER_AESGCM,
+}
+
+var CipherByteObj = map[byte]noise.CipherFunc{
+	NOISE_CIPHER_CHACHAPOLY: noise.CipherChaChaPoly,
+	NOISE_CIPHER_AESGCM:     noise.CipherAESGCM,
+}
+
+// Handshake Patterns
+var PatternStrByte = map[string]byte{
+	"NN": NOISE_PATTERN_NN,
+	"NL": NOISE_PATTERN_NK,
+	"NX": NOISE_PATTERN_NX,
+	"XN": NOISE_PATTERN_XN,
+	"XK": NOISE_PATTERN_XK,
+	"XX": NOISE_PATTERN_XX,
+	"KN": NOISE_PATTERN_KN,
+	"KK": NOISE_PATTERN_KK,
+	"KX": NOISE_PATTERN_KX,
+	"IN": NOISE_PATTERN_IN,
+	"IK": NOISE_PATTERN_IK,
+	"IX": NOISE_PATTERN_IX,
+}
+var PatternByteObj = map[byte]noise.HandshakePattern{
+	NOISE_PATTERN_NN: noise.HandshakeNN,
+	NOISE_PATTERN_NK: noise.HandshakeNK,
+	NOISE_PATTERN_NX: noise.HandshakeNX,
+	NOISE_PATTERN_XN: noise.HandshakeXN,
+	NOISE_PATTERN_XK: noise.HandshakeXK,
+	NOISE_PATTERN_XX: noise.HandshakeXX,
+	NOISE_PATTERN_KN: noise.HandshakeKN,
+	NOISE_PATTERN_KK: noise.HandshakeKK,
+	NOISE_PATTERN_KX: noise.HandshakeKX,
+	NOISE_PATTERN_IN: noise.HandshakeIN,
+	NOISE_PATTERN_IK: noise.HandshakeIK,
+	NOISE_PATTERN_IX: noise.HandshakeIX,
+}
